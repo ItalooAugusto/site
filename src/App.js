7be515119a400cd5dee27fe066f665c4f1e7ba58
Ítalo Augusto <silva.morgado@hotmail.com>
2020-09-29 10:34:52 -0300

    commtis

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 export default function App() {
   return (
     <>
+    <style>
+      background-color: white;
+    </style>
       <h1> Vamos testando os arquivos. </h1>
 
     </>
