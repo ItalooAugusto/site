0eb5e295fa45b1a478350861acd4dc45fcb69417
noiiba <64484209+noiiba@users.noreply.github.com>
2020-09-29 10:38:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React from 'react';
 
 export default function App() {
   return (
-    <>
-    <style>
-      background-color: white;
-    </style>
-      <h1> Vamos testando os arquivos. </h1>
-
-    </>
+      <h1> Testando os arquivos</h1>
   );
 }
