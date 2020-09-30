2acdf99dd3493d14cc2f86cba09c4f9cf30ada87
Ítalo Augusto <silva.morgado@hotmail.com>
2020-09-30 11:41:22 -0300

    commit resolv

--- src/pages/Footer/index.js
+++ src/pages/Footer/index.js
@@ -5,19 +5,6 @@ import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa'
 
 export default function Footer() {
   return (
-    <div className="container-footer">
-    
-    <footer>
-    <div className="testando">
-        <ul>
-        <li> Contato</li>
-        <li> Termos de serviço </li>
-        <li>Política de privacidade</li>
-        <li className="redes-sociais"><FaFacebook size="25" color="blue" /> <FaInstagram size="25" color="fuchsia" /> <FaWhatsapp size="25" color="green" /></li>
-        </ul>
-    </div>
-    </footer>
-
-</div>
+   <h1> Foooter</h1>
   );
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -42,7 +42,7 @@ export default function Main() {
 
       </div>
 
-
+<hr></hr>
 
     </section>
 
